9663e9253c7b7ec6e51a485d550d18ea6fc797d7
joe65477 <j.colavolpe@outlook.com>
2026-05-31 23:30:41 -0400

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+
+
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.echoheartimaging.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
